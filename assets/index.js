1e52f4fb2e676847d1e4c3868bd420229bfb7126
sudharson-31 <sudharsansri105@outlook.com>
2022-05-04 14:33:40 +0530

    Functionality changes
    Added optionSetter() function in JS for displaying options on screen without hardcoding.
    
    Added notification() function to notify when the user tries to add already present item to the list

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,19 @@
 let itemsListDOM = document.getElementById("items-list")
+let optionsListDOM = document.getElementById("price-list")
+let notifyElementDOM = document.getElementById("notification")
 const items = [
     {
+        id: 0,
         serviceName: "Wash Car",
         price: 10 
     },
     {
+        id: 1,
         serviceName: "Mow Lawn",
         price: 20 
     },
     {
+        id: 2,
         serviceName: "Pull Weeds",
         price: 30 
     }
@@ -16,12 +21,30 @@ const items = [
 let totalPrice=0;
 let billList = new Set()
 let listItem =0;
+function optionSetter(){
+    let optionString = ""
+    for(i=0;i<items.length;i++){
+        optionString += `
+        <li class="option" 
+        onclick="handleClick(${(items[i].id)})">
+        ${items[i].serviceName}: $${items[i].price}
+        </li>`
+    }
+    optionsListDOM.innerHTML += optionString;
+}
+optionSetter()
+
 
 function handleClick(id){
     let itemService = items[id].serviceName
     
-    billList.has(itemService)? console.log("already billed"):billList.add(itemService) && createElement(itemService, id);
+    billList.has(itemService)? notification():billList.add(itemService) && createElement(itemService, id);
     
+}
+function notification(){
+    notifyElementDOM.textContent = "Item already added!"
+    notifyElementDOM.style.display="block"
+    setTimeout(()=>{notifyElementDOM.style.display="none"},2000)
 }
 function createElement(itemService, id){
     
